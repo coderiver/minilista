f587613ec6ef3362e48a381aae2127e1c50c1b21
whats0n <mrdvv@mail.ru>
2015-03-13 17:22:09 +0200

    done

--- js/common.js
+++ js/common.js
@@ -14,6 +14,39 @@ head.ready(function() {
 	// $(window).scroll(function(){
 	//     scrollFixedElements()
 	// });
+	var clock;
+	var futureDate  = new Date("March 20, 2015 12:02 PM EDT");
+	var currentDate = new Date();
 
-	console.log($('body').html());
+	// Calculate the difference in seconds between the future and current date
+	var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
+
+	// Calculate day difference and apply class to .clock for extra digit styling.
+	function dayDiff(first, second) {
+		return (second-first)/(1000*60*60*24);
+	}
+
+	if (dayDiff(currentDate, futureDate) < 100) {
+		$('.clock').addClass('twoDayDigits');
+	} else {
+		$('.clock').addClass('threeDayDigits');
+	}
+
+	if(diff < 0) {
+		diff = 0;
+	}
+
+	// Instantiate a coutdown FlipClock
+	clock = $('.clock').FlipClock(diff, {
+		clockFace: 'DailyCounter',
+		language:'ru-ru',
+		countdown: true
+	});
+
+	$('.slider').flickity({
+	  // options
+	  cellAlign: 'left',
+	  wrapAround: true,
+	  contain: true
+	});
 });
--- js/init.js
+++ js/init.js
@@ -1,3 +1,6 @@
 head.load("js/lib/jquery.js", 
 					"js/lib/cycle.js",
+					"js/lib/cycle.carousel.js",
+					"js/lib/flipclock.min.js",
+					"js/lib/flickity.pkgd.min.js",
 					"js/common.js");
